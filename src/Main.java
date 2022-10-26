239230df8cebe5b98434ca27b71e4aa981c89d52
Samuel Sanchez gonzalez <1000068792@hexaware.com>
2022-10-26 11:06:47 -0500

    Mi primer archivo

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// clase-> es una plantilla que contiene elementos, objetos, metodos, funciones, variables.
+// public y class son palabras reservadas
+// Nomenclatura nombre clases -->pascal case -> primer letra empiece con mayuscula
+
+public class Main {
+    // dentro de una clase podemos tener metodos, funciones, variables y constructores
+    public static void main(String[] args) {
+        // linea que nos indica una salida a pantalla
+        // si queremos concatenar algo tenemos que poner el simbolo de +
+        System.out.println("Mi primer ejemplo");
+    }
+}
